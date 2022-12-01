807b64083d39a85306e836b0e288f8d4f1d5791f
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2022-12-01 15:06:56 +0200

    firs commit

---
+++ main.js
@@ -0,0 +1,67 @@
+let canvas = document.querySelector("canvas");
+let cxt = canvas.getContext("2d");
+
+let prevX = null;
+let prevY = null;
+
+cxt.lineWidth = 5;
+let drow = false;
+
+canvas.addEventListener("mousedown", () => {
+    drow = true;
+});
+canvas.addEventListener("mouseup", () => {
+    drow = false;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    // console.log(this);
+    if (prevX == null || prevY == null || !drow) {
+        prevX = e.offsetX;
+        prevY = e.offsetY;
+        return;
+    }
+    let currentX = e.offsetX;
+    let currentY = e.offsetY;
+
+    cxt.beginPath();
+    cxt.moveTo(prevX, prevY);
+    cxt.lineTo(currentX, currentY);
+    // cxt.strokeStyle = color.style.backgroundColor;
+    cxt.stroke();
+
+    prevX = currentX;
+    prevY = currentY;
+});
+
+
+let palitra = document.querySelector(".palitra");
+
+let color;
+
+palitra.addEventListener("click", function active(e) {
+    color = e.target;
+    color.classList.add("active");
+    // color.stopPropagation();
+})
+
+
+let lines = document.querySelector('.lines');
+lines.addEventListener("click", (e)=>{
+    linear = e.target;
+    linear.classList.add("active");
+})
+
+let clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => {
+    cxt.clearRect(0, 0, canvas.width, canvas.height);
+})
+
+let saveBtn = document.querySelector(".save");
+saveBtn.addEventListener("click", () => {
+    let data = canvas.toDataURL("imag/png");
+    let a = document.createElement("a");
+    a.href = data;
+    a.download = "sketch.png";
+    a.click();
+})
